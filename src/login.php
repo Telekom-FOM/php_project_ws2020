e4757efc5c9f7f57e8e1727006d0be288c05fc84
maxbenkert <benkertmax@gmail.com>
2020-11-18 13:20:35 +0000

    mock login

--- src/login.php
+++ src/login.php
@@ -1,1 +1,17 @@
-hallo!
+<html>
+    <body>
+        <form method="POST">
+            <input type="email" name="email" placeholder="email">
+            <input type="submit">
+</form>
+
+<?php
+include("php_include/db_user.php");
+if (isset($_POST["email"])){
+    if(db_check_if_user($_POST["email"])) {
+        echo "User exists!";
+    }
+    else {
+        echo "User does not exist!";
+    }
+}
