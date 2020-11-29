0c19c371df8c170570591d537bdcab27898e7c7d
maxbenkert <benkertmax@gmail.com>
2020-11-29 15:53:27 +0100

    saving whole user object in session

--- src/cart_add.php
+++ src/cart_add.php
@@ -1,8 +1,8 @@
 <?php
-
+require_once("php_include/session.php");
 require_once("php_include/session.php");
 require_once("php_include/db_cart.php");
 
-db_add_to_cart($_SESSION['user'],$_GET['id'], $_GET['amount']);
+db_add_to_cart(unserialize($_SESSION['user'])->kd_nr,$_GET['id'], $_GET['amount']);
 header("Location: /cart.php");
 
